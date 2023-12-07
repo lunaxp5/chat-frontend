ecc4ecb89d6c87f029ace7cb8df6837632a7a796
Jose Luna <lunaxp5@gmail.com>
2023-12-06 23:35:56 -0400

    feat: create messages

--- src/pages/chat/chat.tsx
+++ src/pages/chat/chat.tsx
@@ -2,6 +2,30 @@ import { styled } from "styled-components";
 import Header from "./header";
 import Message from "../../components/message";
 import Footer from "./footer";
+import { useCallback, useEffect, useState } from "react";
+import API from "../../services/api";
+import { AxiosError } from "axios";
+import { ErrorBack } from "../../services/type";
+import { useParams } from "react-router-dom";
+import { Spinner } from "../../components";
+
+interface UserI {
+  _id: string;
+  name: string;
+  email: string;
+}
+
+interface MessageI {
+  sender: UserI;
+  receiver: UserI;
+  description: string;
+  timestamp: Date;
+  read: boolean;
+}
+interface ChatI {
+  participants: string[];
+  messages: MessageI[];
+}
 
 const Wrapper = styled.div`
   width: 100%;
@@ -11,69 +35,109 @@ const Wrapper = styled.div`
   position: relative;
 `;
 const Content = styled.div`
+  height: 100%;
   padding: 8px 4px;
   overflow-y: scroll;
 `;
-const MsgList = [
-  {
-    sender: { $oid: "656f4b9ba5d66e4c5c1dfab5" },
-    receiver: { $oid: "656f52e31af0ab214b1cca6f" },
-    description: "Mensaje 1",
-    read: false,
-    deleted: false,
-    _id: { $oid: "656f9344a50cd19426f9102e" },
-    timestamp: new Date(),
-  },
-  {
-    sender: { $oid: "656f4b9ba5d66e4c5c1dfab5" },
-    receiver: { $oid: "656f52e31af0ab214b1cca6f" },
-    description: "Mensaje 3",
-    read: false,
-    deleted: false,
-    _id: { $oid: "656f9350a50cd19426f91037" },
-    timestamp: new Date(),
-  },
-];
 
 const Chat = () => {
+  const [isLoading, setIsloading] = useState(false);
+  const [user, setUser] = useState<UserI>();
+  const [chat, setChat] = useState<ChatI>();
+  const { chatId } = useParams();
+
+  const getReceiverId = () => {
+    const receiver = chat?.participants.filter(
+      (uid: string) => user && uid !== user._id
+    );
+    if (receiver && receiver?.length > 0) {
+      return receiver[0];
+    }
+    return null;
+  };
+
+  const sendMessage = async (msg: string) => {
+    setIsloading(true);
+    const receiver = getReceiverId();
+    const sender = user && user._id;
+    const payload = {
+      sender,
+      receiver,
+      description: msg,
+    };
+    try {
+      const { data } = await API.post(`/chat/${chatId}/message`, payload);
+      setChat(data);
+    } catch (err) {
+      const error = err as AxiosError;
+      const errorBack = error.response?.data as ErrorBack;
+      alert(errorBack.message);
+    } finally {
+      setIsloading(false);
+    }
+  };
+
+  const getChat = useCallback(async () => {
+    setIsloading(true);
+    try {
+      const { data } = await API.get(`/chat/${chatId}`);
+      setChat(data);
+    } catch (err) {
+      const error = err as AxiosError;
+      const errorBack = error.response?.data as ErrorBack;
+      alert(errorBack.message);
+    } finally {
+      setIsloading(false);
+    }
+  }, [chatId]);
+
+  const isSender = (uidFromMsg: string) => {
+    const userStr = sessionStorage.getItem("user");
+    const user = userStr && JSON.parse(userStr);
+
+    console.log("----------//---------");
+    console.log(user);
+    console.log(uidFromMsg);
+    console.log(user._id === uidFromMsg);
+
+    if (user) {
+      return user._id === uidFromMsg;
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    const userStr = sessionStorage.getItem("user");
+    const userData = userStr && JSON.parse(userStr);
+    setUser(userData);
+
+    getChat();
+  }, [getChat]);
   return (
     <Wrapper>
       <Header />
-
       <Content>
-        <Message
-          author={"Jose Luna"}
-          description={"Hola que tal como estas?"}
-          time={new Date()}
-        />
-        <Message
-          author={"Jose Luna"}
-          description={"Hola que tal como estas?"}
-          time={new Date()}
-        />
-        <Message
-          author={"Jose Luna"}
-          description={"Hola que tal como estas?"}
-          time={new Date()}
-        />
-        <Message
-          author={"Jose Luna"}
-          description={"Hola que tal como estas?"}
-          time={new Date()}
-        />
-        {MsgList.map((item, index) => {
-          return (
-            <Message
-              key={index}
-              sender
-              author={"Jose Luna"}
-              description={item.description}
-              time={item.timestamp}
-            />
-          );
-        })}
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          chat?.messages.map((item: MessageI, index) => {
+            return (
+              <Message
+                key={index}
+                sender={isSender(item.sender._id)}
+                author={item.receiver.name}
+                description={item.description}
+                time={item.timestamp}
+              />
+            );
+          })
+        )}
       </Content>
-      <Footer />
+      <Footer
+        onClick={(msg) => {
+          sendMessage(msg);
+        }}
+      />
     </Wrapper>
   );
 };
--- src/pages/chat/footer.tsx
+++ src/pages/chat/footer.tsx
@@ -1,6 +1,11 @@
 import { styled } from "styled-components";
 import { Button, Input } from "../../components";
 import { IoIosSend } from "react-icons/io";
+import { useState } from "react";
+
+interface FooterI {
+  onClick: (e: string) => void;
+}
 
 const Wrapper = styled.div`
   height: 80px;
@@ -21,11 +26,22 @@ const Icon = styled(IoIosSend)`
   color: ${({ theme }) => theme.lightText};
 `;
 
-const Footer = () => {
+const Footer = ({ onClick }: FooterI) => {
+  const [msg, setMsg] = useState("");
   return (
     <Wrapper>
-      <Input placeholder="Escribir aquí" />
-      <Button rounded>
+      <Input
+        placeholder="Escribir aquí"
+        onChange={(e) => {
+          setMsg(e.target.value);
+        }}
+      />
+      <Button
+        rounded
+        onClick={() => {
+          onClick(msg);
+        }}
+      >
         <Icon />
       </Button>
     </Wrapper>
