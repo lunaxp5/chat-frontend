8e3848af8a5d44ef8b9e9ba8f02b5e48304d5b3e
Jose Luna <lunaxp5@gmail.com>
2023-12-07 09:50:24 -0400

    chore: remove element not used

--- src/components/list-chats.tsx
+++ src/components/list-chats.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import NoChats from "./no-chats";
 import API from "../services/api";
 import { AxiosError } from "axios";
